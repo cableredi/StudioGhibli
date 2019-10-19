7ebf1765276921682bf8e51c4154b720ba471bb9
Kimberly Cable <cableredi@comcast.net>
2019-10-19 17:56:08 -0600

    initial homepage

---
+++ assets/scripts/store.js
@@ -0,0 +1,9 @@
+const STORE = {
+  studioGhibliApi: 'https://ghibliapi.herokuapp.com',
+  theMovieDBApi: 'https://api.themoviedb.org/3/',
+  theMovieDBApiKey: 'a609b5f5061ca98b9c1aa7c216a01d10',
+  theMovieDBImageBase_url: 'https://image.tmdb.org/t/p/',
+  theMovieDBImageBackdrop_size: 'w780',
+  theMovieDBImagePoster_size: 'w780',
+  films: []
+};
