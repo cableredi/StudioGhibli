539c4b3069fd14d1a8d6dcc9a21158eb20f65bd9
Kimberly Cable <cableredi@comcast.net>
2019-10-20 15:22:08 -0600

    home page css changes

--- assets/scripts/store.js
+++ assets/scripts/store.js
@@ -4,6 +4,6 @@ const STORE = {
   theMovieDBApiKey: 'a609b5f5061ca98b9c1aa7c216a01d10',
   theMovieDBImageBase_url: 'https://image.tmdb.org/t/p/',
   theMovieDBImageBackdrop_size: 'w780',
-  theMovieDBImagePoster_size: 'w780',
+  theMovieDBImagePoster_size: 'w342',
   films: []
 };
